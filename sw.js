105b072860af9e2f0fe03cabf12fcad919026ac4
chaejin119 <num1jin@gmail.com>
2026-04-09 15:05:58 +0900

    Build: C.JIN CrossTimer V1.02 Massive UI & Log Restore

--- sw.js
+++ sw.js
@@ -1,13 +1,25 @@
-const CACHE_NAME = 'cjin-timer-v1.01';
+const CACHE_NAME = 'cjin-timer-v1.02';
 const ASSETS = [
   './',
   './index.html',
+  './manifest.json',
   'https://cdn.tailwindcss.com'
 ];
 
 self.addEventListener('install', (e) => {
+  self.skipWaiting();
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(keys.map((key) => {
+        if (key !== CACHE_NAME) return caches.delete(key);
+      }));
+    })
   );
 });
 
